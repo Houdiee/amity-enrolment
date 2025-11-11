8cbf062fa171767a51c7a8583c96d2e8538c3507
Houdiee <>
2025-11-11 15:17:37 +1100

    minor fix up:

--- Api/ApiDbContext.cs
+++ Api/ApiDbContext.cs
@@ -1,9 +1,28 @@
 namespace Api;
 
+using Api.Models;
 using Api.Models.Forms.Enrolment;
 using Microsoft.EntityFrameworkCore;
 
-public class ApiDbContext(DbContextOptions<ApiDbContext> options) : DbContext {
-  public DbSet<EnrolmentForm> Users { get; set; } = null!;
-  public DbSet<EnrolmentForm> EnrolmentForms { get; set; } = null!;
+public class ApiDbContext(DbContextOptions<ApiDbContext> options) : DbContext
+{
+    public DbSet<User> Users { get; set; } = null!;
+    public DbSet<EnrolmentForm> EnrolmentForms { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<User>(user =>
+        {
+            user.HasKey(u => u.Id);
+
+            user.HasIndex(u => u.Email).IsUnique();
+
+            user.HasOne(u => u.EnrolmentForm)
+              .WithOne(ef => ef.User)
+              .HasForeignKey<EnrolmentForm>(ef => ef.UserId)
+              .OnDelete(DeleteBehavior.Cascade);
+        });
+    }
 }
--- Api/Models/User.cs
+++ Api/Models/User.cs
@@ -6,7 +6,5 @@ public class User {
   public int Id { get; set; }
   public required string Email { get; set; }
   public required byte[] HashedPassword { get; set; }
-
-  public int EnrolmentFormId { get; set; }
   public required EnrolmentForm EnrolmentForm { get; set; } = null!;
 }
